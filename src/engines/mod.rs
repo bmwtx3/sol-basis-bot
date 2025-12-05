aba0d671611010e929def459831b762b01036e18
bmwtx3 <bmwertz@gmail.com>
2025-12-05 10:40:51 -0600

    Add engines module placeholder

---
+++ src/engines/mod.rs
@@ -0,0 +1,8 @@
+//! Calculation engines module - Phase 3
+//!
+//! This module will contain:
+//! - Funding engine (funding rate calculation + prediction)
+//! - Basis engine (spread + hedge ratio calculation)
+//! - Signal engine (trade signal generation)
+
+// TODO: Phase 3 implementation
